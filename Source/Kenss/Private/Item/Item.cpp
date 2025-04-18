97b98e401de7a68d5f4366f027ba1fb42c895654
Ken <pagswiss@gmail.com>
2025-04-18 20:31:28 +0200

    Item Klasse gemacht und DebugSachen und Code

---
+++ Source/Kenss/Private/Item/Item.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/Item.h"
+#include "Kenss/DebugSachen.h"
+
+
+// Sets default values
+AItem::AItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+
+    UWorld* World = GetWorld();
+    FVector Location = GetActorLocation();
+    FVector Forward = GetActorForwardVector();
+
+    DRAW_SPHERE(Location);
+    //DRAW_LINE(Location, Location + Forward * 100.f);
+    //DRAW_POINT(Location + Forward * 100.f);
+    DRAW_VECTOR(Location, Location + Forward * 100.f);
+	
+}
+
+// Called every frame
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
